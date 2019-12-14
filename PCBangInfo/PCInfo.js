e817bd8052e5892a24dec0e10bca16f5c9368a31
dev0529 <47568643+dev0529@users.noreply.github.com>
2019-12-15 01:00:54 +0900

    Create PCInfo.js

---
+++ PCBangInfo/PCInfo.js
@@ -0,0 +1,8 @@
+function popmenu(){
+	window.open("PCBangMenu.html",'popmenu','width=410, height=200px');
+}
+
+function popseat(){
+	window.open("PCBangSeat.html",'popseat','width=410, height=110px');
+}
+
