6afeba77e5fb2929cfe4c2aa445fa9955a8b8ae5
sojung127 <ssojung127@gmail.com>
2019-12-15 23:12:07 +0900

    회원가입 시 id 중복확인 기능 작동

---
+++ signup/idDuplic.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="signup.css"; type="text/css">
+        <link rel="stylesheet" href="../menubar/MenuBar.css"; type="text/css">
+    </head>
+    <body>
+    <?php
+        $host='0.tcp.ngrok.io:17235';
+        $sqlUser = 'root';
+        $sqlPW = '1234';
+        $sqlDB='pcreview';
+        $db = new mysqli($host,$sqlUser,$sqlPW,$sqlDB);
+        if(mysqli_connect_errno()){
+            echo 'error try again later';
+            exit;
+        }
+        @session_start();
+
+        $id=$_GET['id'];
+        $query = "SELECT * FROM USERINFO WHERE ID=?;";
+        $stmt=$db->prepare($query);
+        $stmt->bind_param('s',$id);
+        $stmt->execute();
+        $stmt->store_result();
+        $row = $stmt->num_rows;
+        
+        if($row==0){
+            ?>
+	<div><?php echo $id; ?>는 사용가능한 아이디입니다.</div>
+<?php 
+	}else{
+?>
+	<div style='color:red;'><?php echo $id; ?>중복된아이디입니다.<div>
+<?php
+	}
+?>
+<button value="닫기" onclick="window.close()">닫기</button>
+<script>
+</script>
+</body>
+</html>
